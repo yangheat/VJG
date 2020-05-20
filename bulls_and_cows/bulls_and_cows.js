02ba43b1af8206151bc7372c8727f994f23590c2
ParkSun <yangheat@gmail.com>
2020-05-20 18:02:23 +0900

    숫자야구

---
+++ bulls_and_cows/bulls_and_cows.js
@@ -0,0 +1,73 @@
+var body = document.body;
+
+function number_setting() {
+    var number_candidate = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    var answer = []
+    for (var i = 0; i < 3; i++) {
+        /*
+        var number = number_candidate.pop();
+        answer.push(number);
+        //answer = [9, 8, 7, 6]
+        //number_candidate = [1, 2, 3, 4, 5]
+    
+        var number = number_candidate.shift();
+        answer.push(number);
+        //answer = [1, 2, 3, 4]
+        //number_candidate = [5, 6, 7, 8, 9]
+    
+        var number = number_candidate.shift();
+        answer.unshift(number);
+        //answer = [4, 3, 2, 1]
+        //number_candidate = [5, 6, 7, 8, 9]
+        */
+
+        var number = number_candidate.splice(Math.floor(Math.random() * (9 - i)), 1)[0];
+        answer.push(number);
+
+    }
+    console.log(answer);
+    return answer;
+
+}
+
+var answer = number_setting();
+
+var result = document.createElement("h1");
+body.append(result);
+var form = document.createElement("form");
+body.append(form);
+var input = document.createElement("input");
+input.maxLength = 3;
+form.append(input);
+var button = document.createElement("button");
+button.textContent = "입력";
+form.append(button);
+
+// 비동기 콜백함수(Asynchronius Callback function)
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    var strike = 0;
+    var ball = 0;
+    for (var i = 0; i < 3; i++) {
+        for (var j = 0; j < 3; j++) {
+            if (answer[j] === Number(input.value[i])) {
+                if (i === j) {
+                    strike++;
+                } else {
+                    ball++;
+                }
+            }
+        }
+    }
+
+    if (strike === 0 && ball === 0) {
+        result.textContent = "아웃"
+    } else {
+        result.textContent = strike + "S " + ball + "B";
+
+        if(strike === 3){
+            answer = number_setting();             
+        }
+    }
+
+});
