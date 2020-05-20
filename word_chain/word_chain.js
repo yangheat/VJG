ce3e3930ef2b836e40e9199e9ef9df1d70792208
ParkSun <yangheat@gmail.com>
2020-05-20 15:53:00 +0900

    form 추가

--- word_chain/word_chain.js
+++ word_chain/word_chain.js
@@ -1,21 +1,27 @@
 var word = document.createElement("div");
 word.textContent = "제로초";
 document.body.append(word);
+var form = document.createElement("form");
+document.body.append(form);
 var input_box = document.createElement("input");
-document.body.append(input_box);
+form.append(input_box);
 var input_button = document.createElement("button");
 input_button.textContent = "입력";
-document.body.append(input_button);
+form.append(input_button);
 var result_text = document.createElement("div");
 document.body.append(result_text);
 
-input_button.addEventListener("click", function(){
+form.addEventListener("submit", function(e){
+    e.preventDefault();
     if(word.textContent[word.textContent.length-1] === input_box.value[0]){
         result_text.textContent = "정답";
         word.textContent = input_box.value;
         input_box.value = "";
+        input_box.focus();
     } else {
         result_text.textContent = "오답";
+        input_box.value = "";
+        input_box.focus();
     }
 
 });
